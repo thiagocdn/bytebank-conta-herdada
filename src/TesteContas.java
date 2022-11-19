a509c03b495904efd00724f2b68a49a886f67deb
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-11-19 16:43:01 -0300

    Added withdraw method

--- src/TesteContas.java
+++ src/TesteContas.java
@@ -1,2 +1,15 @@
 public class TesteContas {
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(123,123456);
+        cc.deposita(100);
+
+        ContaPoupanca cp = new ContaPoupanca(123,321345);
+        cp.deposita(200);
+
+        cc.transfere(10, cp);
+
+        System.out.println("CC: " + cc.getSaldo());
+        System.out.println("CP: " + cp.getSaldo());
+
+    }
 }
