89b1e7b4e77e4358a71ecc2dc489f742cb8d4909
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-11-19 17:04:43 -0300

    Added abstract class and a method within account Class

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -4,6 +4,12 @@ public class ContaCorrente extends Conta {
     }
 
     @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
+    }
+
+
+    @Override
     public boolean saca(double valor) {
         double valorASacar = valor + 0.20;
         return super.saca(valorASacar);
