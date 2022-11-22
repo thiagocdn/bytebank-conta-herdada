298af32d0bf9015ced2a21aa55961f5b665431c7
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-11-22 20:49:29 -0300

    Created first usable exception example

--- src/TesteContas.java
+++ src/TesteContas.java
@@ -1,5 +1,13 @@
 public class TesteContas {
     public static void main(String[] args) {
+
+        int a = 3;
+        int b = a / 0;
+
+        ContaCorrente outra = null;
+
+        outra.saca(200);
+
         ContaCorrente cc = new ContaCorrente(123,123456);
         cc.deposita(100);
 
