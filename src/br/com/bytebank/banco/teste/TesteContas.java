f2bf8d66700d193b71eb2c35bf383b981a3da252
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-11-25 20:15:22 -0300

    Created a variable visible to the package

--- src/br/com/bytebank/banco/teste/TesteContas.java
+++ src/br/com/bytebank/banco/teste/TesteContas.java
@@ -1,11 +1,15 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.especial.ContaEspecial;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
 
 public class TesteContas {
     public static void main(String[] args) throws SaldoInsuficienteException {
+        ContaEspecial ce = new ContaEspecial(222,333);
+
+
         ContaCorrente cc = new ContaCorrente(123,123456);
         cc.deposita(100);
 
