e4b316987ebd56151a6104bcaf54cb415ff690b9
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-11-26 20:04:15 -0300

    String handling

--- src/br/com/bytebank/banco/teste/TesteString.java
+++ src/br/com/bytebank/banco/teste/TesteString.java
@@ -2,18 +2,51 @@ package br.com.bytebank.banco.teste;
 
 public class TesteString {
     public static void main(String[] args) {
+//        String vazio = " ";
+//        String novoVazio = vazio.trim();
+//        System.out.println(vazio.isEmpty());
+//        System.out.println(novoVazio.isEmpty());
 
-        int a = 3;
-        String nome = "Alura"; // Object Literal
+        String trim = "    ALuRA    ";
+//        String novoTrim = trim.trim();
+//        System.out.println(novoTrim);
+
+        System.out.println(trim.contains("AL"));
+        System.out.println(trim.contains("ALU"));
+
+
+//        int a = 3;
+
+//        String nome = "Alura"; // Object Literal
+//        System.out.println(nome.length());
+//
+//        for(int i = 0; i < nome.length(); i++) {
+//            System.out.println(nome.charAt(i));
+//        }
+
+//        String sub = nome.substring(1);
+//        System.out.println(sub);
+
 //        String nome2 = new String("Alura");
 
-        String nomeReplace = nome.replace("A", "a");
+//        int pos = nome.indexOf("ur");
+//        System.out.println(pos);
 
-        String nomeLower = nome.toLowerCase();
+//        char c = nome.charAt(2);
+//        System.out.println(c);
 
-        System.out.println(nome);
-        System.out.println(nomeReplace);
-        System.out.println(nomeLower);
+//        char c = 'A';
+//        char b = 'a';
+
+//        String nomeReplace = nome.replace(c, b);
+
+//        String nomeLower = nome.toLowerCase();
+//        String nomeUpper = nome.toUpperCase();
+
+//        System.out.println(nome);
+//        System.out.println(nomeReplace);
+//        System.out.println(nomeLower);
+//        System.out.println(nomeUpper);
 
     }
 }
