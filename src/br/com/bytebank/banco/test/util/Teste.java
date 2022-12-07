5fda5ebf7c3e647a06ee13dfb45534f29426066f
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-12-07 18:40:25 -0300

    Created a List of Integer

---
+++ src/br/com/bytebank/banco/test/util/Teste.java
@@ -0,0 +1,23 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teste {
+    public static void main(String[] args) {
+        int[] idades = new int[5];
+        String[] nomes = new String[5];
+
+        int idade = 29;
+        Integer idadeRef = new Integer(29);
+
+        List<Integer> numeros = new ArrayList<>();
+
+        numeros.add(29); // Autoboxing
+        numeros.add(idade);
+        numeros.add(idadeRef);
+
+
+
+    }
+}
